4a19af775c32014b97fdacfbc325237da05f7a8d
mghead <mghead@yandex.ru>
2021-02-26 22:30:20 +0300

    + opencv generate img

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -1,10 +1,13 @@
-#include <iostream>
 #include <ctime>
+#include <iostream>
 #include <vector>
 
+#include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
+
 #ifdef EMSCRIPTEN
-  #include <emscripten.h>
-  #include <emscripten/bind.h>
+#include <emscripten.h>
+#include <emscripten/bind.h>
 #endif
 
 #define int_vector std::vector<int>
@@ -14,35 +17,42 @@ class Process {
   public:
     Process(){};
 
-    int_vector fillarr(int size)
-    {
-        srand((unsigned) time(0));
+    int_vector fillarr(int size) {
+        srand((unsigned)time(0));
         auto arr = int_vector{};
 
-        for(int i=0; i < size; ++i) {
+        for (int i = 0; i < size; ++i) {
             arr.push_back((rand() % 255) + 1);
         }
         return arr;
     }
 
-    int salam(int a)
-    {
-      std::cout << "salam " << a << std::endl;
-      return 0;
+    int_vector randImage() {
+        Mat img(100, 100, CV_8UC3);
+        randu(img, Scalar(0, 0, 0), Scalar(255, 255, 255));
+
+        imshow("random colors image", img);
+        waitKey();
+
+        return 0;
+    }
+
+    int salam(int a) {
+        std::cout << "salam " << a << std::endl;
+        return 0;
     }
 };
 
 int main() {
-  auto p = new Process();
-  auto test = p->fillarr(800 * 600 * 4);
-  for (int i = 0; i < test.size(); i++)
-  {
-    std::cout << test.at(i) << " ";
-  }
+    auto p = new Process();
+    auto test = p->fillarr(800 * 600 * 4);
+    for (int i = 0; i < test.size(); i++) {
+        std::cout << test.at(i) << " ";
+    }
 
-  std::cout << std::endl;
-  
-  return 0;
+    std::cout << std::endl;
+
+    return 0;
 }
 
 #ifdef EMSCRIPTEN
@@ -51,9 +61,9 @@ EMSCRIPTEN_BINDINGS(main) {
     emscripten::smart_ptr_trait<std::shared_ptr<int_vector>>();
 
     emscripten::class_<Process>("Process")
-      .constructor<>()
-      .function("fillarr", &Process::fillarr)
-      .function("salam", &Process::salam);
+        .constructor<>()
+        .function("fillarr", &Process::fillarr)
+        .function("salam", &Process::salam);
 }
 #endif
 
@@ -61,7 +71,7 @@ EMSCRIPTEN_BINDINGS(main) {
 // EMSCRIPTEN_EXPORT
 // void putc_js(char c);
 
-// /* Basic implementation of the writev sys call. */ 
+// /* Basic implementation of the writev sys call. */
 // EMSCRIPTEN_KEEPALIVE
 // size_t writev_c(int fd, const struct iovec *iov, int iovcnt) {
 //   size_t cnt = 0;
